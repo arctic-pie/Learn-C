488e33088c208a47ace6521ef5433c0179c4b3ec
Ksenia Grigoryeva <ksu.la.fleur@gmail.com>
2018-03-17 02:35:57 +0300

    Lab 1.3

--- Lab 1.3/temperature.c
+++ Lab 1.3/temperature.c
@@ -2,11 +2,13 @@
 /* печать таблицы температур по Фаренгейту и Цельсию для fahr = 0, 20 ... 300; вариант с плавающей точкой */
 main() {
     float fahr, celsius;
-    int lower, upper; step;
+    int lower, upper, step;
     lower = 0; /* нижняя граница таблицы температур */
     upper = 300; /* верхняя граница */
     step = 20; /* шаг */
     fahr = lower;
+    printf ("Таблица соответситвия температур:\n");
+    printf ("%3s %3s\n", "F", "C");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf ("%3.0f %6.1f\n", fahr, celsius);

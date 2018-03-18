5463158b3183d73090e32dbe1b352bb1e1f5e3b4
Ksenia Grigoryeva <ksu.la.fleur@gmail.com>
2018-03-18 05:02:08 +0300

    Update lab 1.3

--- lab1.3/Lab/Lab/main.c
+++ lab1.3/Lab/Lab/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+
 /* печать таблицы температур по Фаренгейту и Цельсию для fahr = 0, 20 ... 300; вариант с плавающей точкой */
-main() {
+int main() {
     float fahr, celsius;
     int lower, upper, step;
+    
     lower = 0; /* нижняя граница таблицы температур */
     upper = 300; /* верхняя граница */
     step = 20; /* шаг */
     fahr = lower;
-    printf ("Таблица соответситвия температур:\n");
-    printf ("%3s %3s\n", "F", "C");
+    
+    // header
+    printf("Temperature matching table: \n");
+    printf("%3s %6s\n", "F", "C");
+    
     while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf ("%3.0f %6.1f\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
-} }
+    }
+}

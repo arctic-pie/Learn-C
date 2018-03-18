4fbebc4fa10e41babea52f5ad2a99639c3a66dc9
Ksenia Grigoryeva <ksu.la.fleur@gmail.com>
2018-03-18 03:55:04 +0300

    Update

--- hello.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-main()
-{
-    printf ("Большой кусь!\n");
-}
---
+++ lab1.5/Lab/Lab/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab
+//
+//  Created by Ksenia Grigoryeva on 18/03/2018.
+//  Copyright © 2018 Ksenia Grigoryeva. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

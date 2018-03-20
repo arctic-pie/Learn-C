e606dcc57da284e245fc7031b2b9eca265b7b2bd
Ksenia Grigoryeva <ksu.la.fleur@gmail.com>
2018-03-20 23:00:42 +0300

    Lab 1.5

--- lab1.5/Lab/Lab/main.c
+++ lab1.5/Lab/Lab/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    for (int a = 0; a < 5; a++) {
-        printf("a");
-        printf("b");
-    }
+/* печать таблицы температур по Фаренгейту и Цельсию от 300 дл 0 */
+
+int main() {
+    int fahr;
     
-    return 0;
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf ("%3d %6.1f\n", fahr, (5.0 / 9.0) * ( fahr - 32));
 }

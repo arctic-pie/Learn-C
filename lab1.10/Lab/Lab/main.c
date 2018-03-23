3305c76e8b5918aa23b8731992d3a8292d05a3f0
Ksenia Grigoryeva <ksu.la.fleur@gmail.com>
2018-03-23 04:28:11 +0300

    Lab 1.10

---
+++ lab1.10/Lab/Lab/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* копирование ввода на вывод с заменой символа табуляции на \t, символа забоя на \b и каждой обратной наклонной черты на \\ */
+
+// Алгоритм
+
+// Считываем символ
+// Если равен символу табуляции, выводим \t
+// Если равен символу забоя, выводим \b
+// Если это \, выводим \\
+// В остальных случаях выводим исходный символ
+
+int main() {
+    printf("To terminate a program, press \"Ctrl\" + \"D\"\nInsert text:\n");
+    
+    char character;
+    
+    while ((character = getchar()) != EOF) {
+        if (character == '\t') {
+            printf("\\t");
+        } else if (character == '\b') {
+            printf("\\b");
+        } else if (character == '\\') {
+            printf("\\\\");
+        } else {
+            putchar(character);
+        }
+    }
+// >> не заменяет backspace, завершает по enter
+}
